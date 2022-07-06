d0ce7324b909616efb1dce5579f33335aa5a4d52
Wolfeye78 <magnus.petersson.78@gmail.com>
2022-07-06 19:09:04 +0200

    Data stored as json in databank, for each activation a new post is stored.

--- src/main.lua
+++ src/main.lua
@@ -1,69 +1,68 @@
-prefix = "Counter" --export: All elements that should be included must have this prefix in the name.
-databank = "Values" --export: The element name of the databank that stores all data from the counters.
+Prefix = "Counter" --export: All elements that should be included must have this prefix in the name.
+Databank = "Values" --export: The element name of the databank that stores all data from the counters.
 
-local core = library.getCoreUnit()
-local db = library.getLinkByName(databank)
+local Core = library.getCoreUnit()
+local Db = library.getLinkByName(Databank)
 local RawTime = system.getUtcTime()
 local ElementName = ""
 local ElementId = ""
 local IndustryInfo = {}
-local UnitsProduced = 0
+local TotalUnitsProduced = 0
+--local DeltaUnitsProduced = 0
 
-All_Elements_Ids = core.getElementIdList()
+All_Elements_Ids = Core.getElementIdList()
 
--- Get the first part of a string
+--Db.clear()
+
+-- Get the first part of a string, case insensitive
 function starts_with(String,Start)
     return string.sub(string.lower(String),1,string.len(string.lower(Start))) == string.lower(Start)
 end
 
 -- Create a matrix of element id's and names, but only for names starting with the prefix
-Counters = {}
+local AllCountersData = {}
+local AllCountersData_Key = 0
 for i in pairs(All_Elements_Ids) do
     ElementId = All_Elements_Ids[i]
-    ElementName = core.getElementNameById(ElementId)
-    if starts_with(ElementName,prefix) then
-        Counters[i] = {}
-        Counters[i][1] = RawTime
-        Counters[i][2] = ElementId
-        Counters[i][3] = ElementName
-        IndustryInfo = core.getElementIndustryInfoById(ElementId)
-        UnitsProduced = IndustryInfo.unitsProduced
-        Counters[i][4] = UnitsProduced
-
+    ElementName = Core.getElementNameById(ElementId)
+    if starts_with(ElementName, Prefix) then
+        AllCountersData_Key = AllCountersData_Key + 1
+        AllCountersData[AllCountersData_Key] = {}
+        AllCountersData[AllCountersData_Key][1] = ElementId
+        IndustryInfo = Core.getElementIndustryInfoById(ElementId)
+        TotalUnitsProduced = IndustryInfo.unitsProduced
+        AllCountersData[AllCountersData_Key][2] = TotalUnitsProduced
     end
 end
+--[[
+-- Check if production is reset because of element restart
+if Db.getNbKeys() > 0 then
+    local LastDbLine = json.decode(Db.getStringValue(Db.getNbKeys()))
+    for i in pairs(AllCountersData) do
+        if LastDbLine[2][i][2] > TotalUnitsProduced then
+            DeltaUnitsProduced = TotalUnitsProduced
+        else
+            DeltaUnitsProduced = TotalUnitsProduced - LastDbLine[i][2]
+        end
+    end
+end
+]]
 
 -- Format RawTime to "date time".
 
--- Convert to json
-local dataAsJson = json.encode(Counters)
-
 -- Write to Databank
-db.clear()
-db.setStringValue(db.getNbKeys() + 1,dataAsJson)
+local NewDbLine = {RawTime, AllCountersData}
+Db.setStringValue(Db.getNbKeys() + 1,json.encode(NewDbLine))
 
 -- Print to screen for debugging
 system.print(" - - - - - - - - - - - - - - - - - - -")
---[[system.print("Data from table:")
-for i in pairs(Counters) do
-    system.print("Time: " .. Counters[i][1])
-    system.print("Id: " .. Counters[i][2])
-    system.print("Name: " .. Counters[i][3])
-    system.print("Produced: " .. Counters[i][4])
-end
-]]
 system.print("Data from databank:")
-for i=1,db.getNbKeys() do
-    local dataAsTable = json.decode(db.getStringValue(i))
-    for j in pairs(dataAsTable) do
-        system.print("Time: " .. dataAsTable[j][1])
-        system.print("Id: " .. dataAsTable[j][2])
-        system.print("Name: " .. dataAsTable[j][3])
-        system.print("Produced: " .. dataAsTable[j][4])
+local DbLine = {}
+for i=1, Db.getNbKeys() do
+    DbLine = json.decode(Db.getStringValue(i))
+    system.print("---- Time: " .. DbLine[1])
+    for j in pairs(DbLine[2]) do
+        system.print("Id: " .. DbLine[2][j][1] .. " --- " .. "Produced: " .. DbLine[2][j][2])
     end
 
 end
-
-
---Industry = core.getElementIndustryInfoById(5)
---system.print(Industry.unitsProduced)
